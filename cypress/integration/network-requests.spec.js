d0438dbf5ec232a972cded7087e216ceb6cd825f
Robert Guss <robertguss@pm.me>
2021-10-05 09:31:14 -0400

    added network test and posts.json fixture

--- cypress/integration/network-requests.spec.js
+++ cypress/integration/network-requests.spec.js
@@ -1,3 +1,12 @@
 describe("Intercepting Network Requests", () => {
-  it("", () => {});
+  it("/api/posts returns a status code of 200", () => {
+    // Write an assertion that the route '/api/posts'
+    // returns a status code of 200
+    // Hint: You will need to use cy.request()
+    // https://docs.cypress.io/api/commands/request
+
+    cy.request("GET", "http://localhost:3000/api/posts").then((response) => {
+      expect(response.status).to.eq(200);
+    });
+  });
 });
