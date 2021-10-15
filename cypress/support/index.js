543466e49004dbc6fa558d67979602959170e9e3
Robert Guss <robertguss@pm.me>
2021-10-15 11:45:22 -0400

    import answers/commands.js into support/index.js

--- cypress/support/index.js
+++ cypress/support/index.js
@@ -14,7 +14,8 @@
 // ***********************************************************
 
 // Import commands.js using ES2015 syntax:
-import './commands'
+import "./commands";
+import "./Answers/commands";
 
 // Alternatively you can use CommonJS syntax:
 // require('./commands')
